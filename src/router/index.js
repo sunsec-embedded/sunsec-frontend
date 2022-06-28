fe2b9ada1dfe5beaa6ca543b4e7615f1db6fde67
PleBea <bini060315kim@gmail.com>
2022-06-29 03:52:42 +0900

    chage: router

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MainPage from '@/views/MainPage'
+import LoginPage from '@/views/LoginPage'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'MainPage',
+    component: MainPage
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/login',
+    name: 'LoginPage',
+    component: LoginPage
   }
 ]
 
