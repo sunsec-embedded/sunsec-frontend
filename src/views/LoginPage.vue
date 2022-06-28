11ed88f17e32f1488ce70abd4b8eff1b79bb806c
PleBea <bini060315kim@gmail.com>
2022-06-29 03:52:51 +0900

    Create LoginPage.vue

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,33 @@
+<template>
+    <!-- 백그라운드 이미지 -->
+    <img src="../assets/Background.svg" alt="BackgroundImg">
+
+    <!-- 로그인 컨포넌트 -->
+    <div id="login-box">
+        <LoginBox></LoginBox>
+    </div>
+</template>
+
+<script>
+import LoginBox from '@/components/LoginPage/LoginBox.vue'
+
+export default {
+    components: {
+        LoginBox
+    }
+}
+</script>
+
+<style>
+img{
+    margin-left: auto;
+    display: flex;
+    height: 100vh;
+}
+
+#login-box{
+    position: absolute;
+    top: 22%;
+    left: 15%;
+}
+</style>
